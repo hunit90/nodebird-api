7a3d3990013e33ebe06a60585f5981e903597ed5
hunit <hunit612@naver.com>
2023-09-24 23:22:25 +0900

    feat: jwt 인증 기능 추가

---
+++ controllers/v1.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const { Domain, User } = require('../models');
+
+exports.createToken = async (req, res) => {
+    const { clientSecret } = req.body;
+    try {
+        const domain = await Domain.findOne({
+            where: { clientSecret },
+            include: {
+                model: User,
+                attributes: ['nick', 'id'],
+            },
+        });
+        if (!domain) {
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+            });
+        }
+        const token = jwt.sign({
+            id: domain.User.id,
+            nick: domain.User.nick,
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1m', // 1분
+            message: '토큰이 발급되었습니다',
+            token,
+        });
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({
+            code: 500,
+            message: '서버에러',
+        });
+    }
+};
+
+exports.tokenTest = (req, res) => {
+    res.json(res.locals.decoded);
+}
